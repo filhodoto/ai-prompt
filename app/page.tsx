e1554ddc2b1922c6ded926f16ae3d0304d3f9609
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-04-08 14:29:44 +0100

    Add first text to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share <br />
+        <span className="blue_gradient">AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
