686922e3743a38b540b04f86c8e6756e5441beb0
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-04-09 11:21:47 +0100

    Add signin logic in NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,50 @@
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+interface ProfileProps {
+  name: string;
+  email: string;
+  picture: string;
+}
+
+/* More about session and signIn callbacks here:
+https://next-auth.js.org/getting-started/example#extensibility */
 
 const handler = NextAuth({
   // Array with the providers we will handle
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   session: async ({ session }) => {},
-  signIn: async ({ profile }) => {
+  signIn: async ({ profile }: { profile: ProfileProps }) => {
     try {
+      await connectToDB();
+
+      // 1. Check if user already exists
+      const user = await User.findOne({ email: profile.email });
+      // 2. If not, create new user
+      if (!user) {
+        // TODO:: Make sure this works and probably make this check on frontend
+        const sanitizedName = profile.name.replace(' ', '').toLowerCase();
+
+        User.create({
+          email: profile.email,
+          username: sanitizedName,
+          image: profile.picture,
+        });
+      }
+
+      return true;
     } catch (error) {
-      console.log('ERROR Sign in:: ', error);
+      console.log('ERROR signin in:: ', error);
+
+      return false;
     }
   },
 });
