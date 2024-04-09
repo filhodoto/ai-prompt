64ca101ac579118b84588dce4bcf74ba03159e9e
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-04-09 16:33:20 +0100

    Create base create-post structure

---
+++ app/create-post/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Form from '@components/Form';
+import React, { useState } from 'react';
+
+export interface PostProps {
+  prompt: string;
+  tag: string;
+}
+
+const defaultPost = {
+  prompt: '',
+  tag: '',
+};
+
+// TODO:: Use React Form for the form in the future
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState();
+  const [post, setPost] = useState(defaultPost);
+
+  const createPost = async (e) => {};
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={createPost}
+      submitting
+      handleSubmit={createPost}
+    />
+  );
+};
+
+export default CreatePrompt;
