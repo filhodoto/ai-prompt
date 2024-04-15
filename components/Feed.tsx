35d8baf7c81229f16fb1b20d5b2edc63871b35a1
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-04-15 11:37:37 +0100

    retrieve all posts on Feed component mount

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+
+const GET_POSTS_API = '/api/posts';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      // Get all posts
+      const response = await fetch(GET_POSTS_API);
+
+      // If post was created correctly we navigate user to homepage
+      if (response.ok) {
+        const posts = await response.json();
+        console.log(posts);
+        // setState
+        setPosts(posts);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return <div>Feed</div>;
 };
 
