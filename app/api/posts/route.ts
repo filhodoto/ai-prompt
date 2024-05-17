3575c9c0cb2a9b9b04d888b14f67a1e543caccef
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-05-17 14:31:30 +0100

    Improve search Endpoint - Filter posts by creator name - Improve comparision by adding case insensitive regex

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,16 @@
 import Post from '@models/post';
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse } from 'next';
+import Error from 'next/error';
 import { NextResponse, NextRequest } from 'next/server';
+
+// Get ID of users with username contains the filter text
+const getUserIdsByFilter = async (regexFilter: RegExp): Promise<string[]> => {
+  return (await User.find({ username: { $regex: regexFilter } })).map(
+    (user) => user._id
+  );
+};
 
 // Get posts. If a search param is required we filter them, if not we return all
 export const GET = async (req: NextRequest, res: NextApiResponse) => {
@@ -9,27 +18,39 @@ export const GET = async (req: NextRequest, res: NextApiResponse) => {
     // TODO:: This is duplicated in all routes, should create a middleware for this
     await connectToDB();
 
+    let allPosts;
+
     // Check if there is a filter value
     /* !! NOTE:: This is specific to NextJS. In a normal API qe could do const {filter} = req.query
     https://github.com/vercel/next.js/discussions/47072#discussioncomment-5291376 !! */
     const filterVal = req.nextUrl.searchParams.get('filter');
 
-    // Define DB query depending if we have a filter or not
-    const query = filterVal
-      ? {
-          $or: [
-            { prompt: { $regex: filterVal } },
-            { tag: { $regex: filterVal } },
-          ],
-        }
-      : {};
+    // If no filter value is passed, return all posts
+    if (!filterVal) {
+      // Get all posts (populate "creator" key which we create on "api/post/new")
+      allPosts = await Post.find({}).populate('creator').exec();
+    } else {
+      const regexFilter = new RegExp(filterVal, 'i');
 
-    // Get all posts (populate "creator" key which we create on "api/post/new")
-    const allPosts = await Post.find(query).populate('creator').exec();
+      // Define DB query depending if we have a filter or not
+      const query = {
+        $or: [
+          { prompt: { $regex: regexFilter } },
+          { tag: { $regex: regexFilter } },
+          { creator: { $in: await getUserIdsByFilter(regexFilter) } },
+        ],
+      };
+
+      // Get all posts (populate "creator" key which we create on "api/post/new")
+      allPosts = await Post.find(query).populate('creator').exec();
+    }
 
     // Return all posts
     return NextResponse.json(allPosts, { status: 200 });
   } catch (error) {
-    return new Response('Failed retrieving all posts', { status: 500 });
+    console.error(error);
+    return new Response(`Failed retrieving posts: ${error.message || error}`, {
+      status: 500,
+    }); // Handle errors gracefully
   }
 };
